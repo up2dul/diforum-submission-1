55c7a4d987626ba05bace7d0aa49d11c3cf8f372
abdul <malik301002@gmail.com>
2022-11-29 13:01:16 +0700

    feat: setup tailwindcss and apply new font-family

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      'dark': '#020302',
+      'dark-2': '#323232',
+      'light': '#FEFEFE',
+      'light-2': '#F8F8F8',
+      'sky': '#83D2F2',
+      'yellow': '#F8E34F'
+    }
+  },
+  plugins: []
+}
