039149585f4992825cff3c09b2525fc2acf8b030
abdul <malik301002@gmail.com>
2022-11-30 22:05:53 +0700

    refactor(comps): separate Header components from Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header className='fixed top-0 z-30 flex w-full items-center justify-center border-b-2 border-dark bg-light py-3'>
+    <Link to='/'>
+      <h1 className='text-2xl font-medium text-dark'>Diforum</h1>
+    </Link>
+  </header>
+);
+
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,9 @@
-import { Link } from 'react-router-dom';
-
 import { navItemList } from '@/utils';
-import Button from './Button';
 import NavItem from './NavItem';
 
 const Navbar = () => (
-  <nav className='x-container fixed bottom-0 z-30 w-full border-y-2 border-b-dark bg-light py-6 md:bottom-auto'>
-    <div className='hidden w-full items-center justify-between md:flex'>
-      <Link to='/'>
-        <h1 className='text-2xl font-medium text-dark'>Diforum</h1>
-      </Link>
-
-      <Button>My profile</Button>
-    </div>
-
-    <div className='flex items-center justify-between text-2xl text-dark md:hidden'>
+  <nav className='x-container fixed bottom-0 z-30 w-full border-t-2 border-b-dark bg-light py-5'>
+    <div className='flex items-center justify-between text-2xl text-dark'>
       {navItemList.map((props, idx) => (
         <NavItem key={idx} {...props} />
       ))}
