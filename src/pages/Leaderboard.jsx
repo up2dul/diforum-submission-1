36a846df3cce1f0728866861ca2d24f9284e2f20
abdul <malik301002@gmail.com>
2022-12-01 01:22:21 +0700

    feat(pages): populate leaderboard data to Leaderboard page

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,11 +1,46 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { asyncPopulate } from '@/states/shared/action';
 import Layout from '@/components/Layout';
 
-const Leaderboard = () => (
-  <Layout>
-    <section className='x-container'>
-      <h1>leaderboard page</h1>
-    </section>
-  </Layout>
-);
+const Leaderboard = () => {
+  const { leaderboard: { leaderboards } } = useSelector((states) => states);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPopulate());
+  }, [dispatch]);
+
+  return (
+    <Layout>
+      <h1>🏆 Active users leaderboard</h1>
+
+      <div className='mt-8 flex flex-col gap-y-4'>
+        <div className='flex justify-between'>
+          <h2>User</h2>
+
+          <h2>Score</h2>
+        </div>
+
+        {leaderboards.map(({ user: { name, avatar }, score }) => (
+          <article key={avatar} className='flex justify-between'>
+            <div className='flex items-center gap-5'>
+              <img
+                src={avatar}
+                alt={name + ' ava'}
+                className='w-11 rounded-xl'
+              />
+
+              <h2>{name}</h2>
+            </div>
+
+            <h2>{score}</h2>
+          </article>
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default Leaderboard;
