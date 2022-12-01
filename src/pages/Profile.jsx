ff2ab1c63b1bedae509d90346ad182085ab0b7cc
abdul <malik301002@gmail.com>
2022-12-01 13:59:05 +0700

    feat(pages): temporary content in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,23 @@
+import Button from '@/components/Button';
 import Layout from '@/components/Layout';
 
 const Profile = () => (
   <Layout>
-    <section className='x-container'>
-      <h1>profileeee page</h1>
-    </section>
+    <h1>🙂 Profile</h1>
+
+    <div className='my-8 flex flex-col items-center gap-y-3'>
+      <img
+        src='https://ui-avatars.com/api/?name=Mark&background=random'
+        alt='My ava'
+        className='w-16 rounded-xl'
+      />
+
+      <h1>Mark zukiberh</h1>
+
+      <h2 className='mb-8'>mark@facebook.com</h2>
+
+      <Button>Log out</Button>
+    </div>
   </Layout>
 );
 
