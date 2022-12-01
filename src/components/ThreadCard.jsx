493ad714beeb37c7c6fe5e610fcfb02dce76e2ca
abdul <malik301002@gmail.com>
2022-12-01 13:57:25 +0700

    feat(comps): add some icons and edit some className

--- src/components/ThreadCard.jsx
+++ src/components/ThreadCard.jsx
@@ -1,25 +1,30 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { HiOutlineChat, HiOutlineClock, HiOutlineUser } from 'react-icons/hi';
 
-import { commentsCount, postedAt } from '@/utils';
+import { postedAt } from '@/utils';
 
 const ThreadCard = ({ id, title, body, category, user, createdAt, totalComments }) => (
   <Link to={'thread/' + id}>
-    <article className='overflow-hidden rounded-xl border border-dark hover:outline outline-2'>
-      <div className='border-b border-dashed border-dark bg-sky p-5 pb-3'>
+    <article title={title} className='overflow-hidden rounded-xl border-2 border-dark bg-light hover:border-dashed'>
+      <div className='border-b border-dashed border-dark p-5 pb-3'>
         <h1 className='truncate font-medium'>{title}</h1>
       </div>
 
       <p className='mx-5 my-3 truncate'>{body}</p>
 
       <div className='mx-5 mb-5 flex flex-wrap justify-between gap-x-4 gap-y-2 text-sm'>
-        <p>{commentsCount(totalComments)}</p>
-
         <p>
-          Posted by <span className='font-medium'>{user}</span>
+          <HiOutlineChat className='-mt-1 inline text-lg' /> {totalComments}
         </p>
 
-        <p>{postedAt(createdAt)}</p>
+        <p>
+          <HiOutlineUser className='-mt-1 inline text-lg' /> {user}
+        </p>
+
+        <p>
+          <HiOutlineClock className='-mt-1 inline text-lg' /> {postedAt(createdAt)}
+        </p>
 
         <p className='font-medium'>#{category}</p>
       </div>
