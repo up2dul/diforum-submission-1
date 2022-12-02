027d35ad35eafbc1ea533fa9ba9d3aa30cfe22d8
abdul <malik301002@gmail.com>
2022-12-02 22:11:39 +0700

    refactor(comps): add optional withBackButton props in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom';
+import { HiArrowLeft } from 'react-icons/hi';
+import PropTypes from 'prop-types';
 
-const Header = () => (
-  <header className='fixed top-0 z-30 flex w-full items-center justify-center border-b-2 border-dark bg-light py-3'>
+const Header = ({ withBackButton = false }) => (
+  <header className='x-container fixed top-0 z-30 flex w-full items-center gap-x-6 border-b-2 border-dark bg-light py-3'>
+    {withBackButton && (
+      <Link to='/' title='Back to home'>
+        <HiArrowLeft className='text-2xl' />
+      </Link>
+    )}
+
     <Link to='/'>
-      <h1 className='font-medium text-dark'>Diforum</h1>
+      <h2 className='font-medium text-dark'>💬 Diforum</h2>
     </Link>
   </header>
 );
+
+Header.propTypes = {
+  withBackButton: PropTypes.bool
+};
 
 export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,23 +3,24 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import Navbar from './Navbar';
 
-const Layout = ({ isFluidContainer, children }) => {
+const Layout = ({ withBackButton, isFluidContainer = false, children }) => {
   const containerClass = isFluidContainer ? 'x-fluid-container' : 'x-container';
 
   return (
     <>
-      <Header />
+      <Header withBackButton={withBackButton} />
 
       <Navbar />
 
-      <main className={containerClass + ' pb-32 pt-24'}>{children}</main>
+      <main className={`${containerClass} pb-32 pt-24`}>{children}</main>
     </>
   );
 };
 
 Layout.propTypes = {
+  withBackButton: PropTypes.bool,
   isFluidContainer: PropTypes.bool,
-  children: PropTypes.node
+  children: PropTypes.node.isRequired
 };
 
 export default Layout;
