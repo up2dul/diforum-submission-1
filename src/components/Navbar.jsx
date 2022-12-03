f17fb4126ec3a0f3ced78ea361151ff6881e8109
abdul <malik301002@gmail.com>
2022-12-03 19:29:17 +0700

    refactor(comps): dynamic NavItem based on authUser

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,33 @@
-import { navItemList } from '@/utils';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { HiChartBar, HiOutlineHome, HiOutlineLogin, HiOutlineUser } from 'react-icons/hi';
+
+import { asyncPreloadProcess } from '@/states/is-preload/action';
 import NavItem from './NavItem';
 
-const Navbar = () => (
-  <nav className='x-container fixed bottom-0 z-30 w-full border-t-2 border-b-dark bg-light py-5'>
-    <div className='flex items-center justify-between text-2xl text-dark'>
-      {navItemList.map((props, idx) => (
-        <NavItem key={idx} {...props} />
-      ))}
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const { authUser } = useSelector((states) => states);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPreloadProcess());
+  }, [dispatch]);
+
+  return (
+    <nav className='x-container fixed bottom-0 z-30 w-full border-t-2 border-b-dark bg-light py-5'>
+      <div className='flex items-center justify-between text-2xl text-dark'>
+        <NavItem to='/' title='Home' icon={<HiOutlineHome />} />
+
+        <NavItem to='/leaderboard' title='Leaderboard' icon={<HiChartBar />} />
+
+        {authUser === null ? (
+          <NavItem to='/login' title='Login' icon={<HiOutlineLogin />} />
+        ) : (
+          <NavItem to='/profile' title='My profile' icon={<HiOutlineUser />} />
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
