0e862e5064dc980cae76f60a58e0bbcdd01259c2
abdul <malik301002@gmail.com>
2022-12-03 19:16:01 +0700

    feat(comps): add type props to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ title = 'Button', children, onClick }) => (
-  <button type='button' title={title} className='button' onClick={onClick}>
+const Button = ({ type = 'button', title, children, onClick }) => (
+  // eslint-disable-next-line react/button-has-type
+  <button type={type} title={title} className='button' onClick={onClick}>
     {children}
   </button>
 );
 
 Button.propTypes = {
+  type: PropTypes.oneOf(['button', 'submit']),
   title: PropTypes.string,
   children: PropTypes.string.isRequired,
   onClick: PropTypes.func
