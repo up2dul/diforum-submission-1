92fc35fa5da79faaf5fb923ec1cebf084ec467de
abdul <malik301002@gmail.com>
2022-12-03 21:54:02 +0700

    feat(pages): login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { asyncPreloadProcess } from '@/states/is-preload/action';
+import { asyncSetAuthUser } from '@/states/auth-user/action';
 import Layout from '@/components/Layout';
 import Input from '@/components/Input';
 import Button from '@/components/Button';
 
 const Login = () => {
-  const { authUser } = useSelector((states) => states);
+  const { isPreload } = useSelector((states) => states);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const formRef = useRef(null);
 
   useEffect(() => {
     dispatch(asyncPreloadProcess());
   }, [dispatch]);
 
-  if (authUser) navigate('/profile');
+  if (isPreload) return null;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const email = formRef.current[0].value;
+    const password = formRef.current[1].value;
+
+    dispatch(asyncSetAuthUser({ email, password }));
+    navigate('/profile');
+  };
 
   return (
     <Layout>
       <h2>📲 Log in to your account</h2>
 
-      <form className='mx-auto mt-8 flex max-w-md flex-col items-center justify-center gap-y-6'>
+      <form
+        ref={formRef}
+        className='mx-auto mt-8 flex max-w-md flex-col items-center justify-center gap-y-6'
+        onSubmit={handleSubmit}
+      >
         <Input
           title='Email address'
           type='email'
